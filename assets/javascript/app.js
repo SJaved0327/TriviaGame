9dd08e9d6b206f9d7a69fb6160b252efb9c8c86e
Sara Javed <sarajaved@Saras-MacBook-Air.local>
2017-09-16 16:59:03 -0400

    added basic Bootstrap framework and test questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+//Global Variables
+//----------------------------------------
+
+
+
+//Functions
+//----------------------------------------
+
+
+
+
+
+
+//Gameplay
+//----------------------------------------
+
+//intro page displays
+
+//click start to reveal gameplay page
+
+//main page with questions
+	//Question
+	//Answers 1 - 4
+
+//user selects radio buttons to choose answer
+//user can only select one answer at a time
+
+//hit Done button when done to go immediately to Done page
+
+//time counts down 1 second at a time
+//decrement 1000
+
+//when time = 0, Done page displays
+	// All Done!
+	// Correct Answers = 0
+	// Incorrect Answers = 0
+	// Unanswered = 0
