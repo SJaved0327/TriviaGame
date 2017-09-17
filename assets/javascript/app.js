800ba6f622989bad7e1234ab59c090e86bb58ed0
Sara Javed <sarajaved@Saras-MacBook-Air.local>
2017-09-16 20:52:53 -0400

    if else statements are functioning. removing console logs.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,9 @@
 //Global Variables
 //----------------------------------------
+var correct = 0;
+var wrong = 0;
+var unanswered = 0;
+
 
 
 //Functions
@@ -14,8 +18,49 @@
 //When game is complete
 
 $("#doneButton").click(function(){
+	
+	//check value of question 1
 	var question1 = document.questionDiv.question1.value;
-	console.log(question1);
+	
+	//check value of question 2
+	var question2 = document.questionDiv.question2.value;
+	
+	//check value of question 3
+	var question3 = document.questionDiv.question3.value;
+
+	//Question 1 Check
+	if (question1 === "correct"){
+		correct++;
+	}
+	else if (question1 === "wrong"){
+		wrong++;
+	}
+	else {
+		unanswered++;
+	};
+
+	//Question 2 Check
+	if (question2 === "correct"){
+		correct++;
+	}
+	else if (question2 === "wrong"){
+		wrong++;
+	}
+	else {
+		unanswered++;
+	};
+
+	//Question 3 Check
+	if (question3 === "correct"){
+		correct++;
+	}
+	else if (question3 === "wrong"){
+		wrong++;
+	}
+	else {
+		unanswered++;
+	};
+
 
 });
 
