bc43df4781f8973dbd90f4099af88b44ee9b50ed
Sara Javed <sarajaved@Saras-Air.fios-router.home>
2017-09-17 21:46:59 -0400

    saving before doing major css edits

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,6 +21,7 @@ function StartGame() {
 
 	intervalId = setInterval(decrement, 1000);
 	$("#questionDiv").css('visibility', 'visible');
+	$("#doneButton").css('visibility', 'visible');
 };
 
 //Decrement function starts at number (30) and decreases number by 1 until 0
@@ -58,6 +59,12 @@ function EndGame(){
 	
 	//check value of question 3
 	var question3 = document.questionDiv.question3.value;
+
+	//check value of question 4
+	var question4 = document.questionDiv.question4.value;
+
+	//check value of question 5
+	var question5 = document.questionDiv.question5.value;
 
 	//---Question 1 Check---
 	//if value of question1 is correct:
@@ -110,28 +117,79 @@ function EndGame(){
 		counter.unanswered++;
 	};
 
+	//---Question 4 Check---
+	//if value of question4 is correct:
+	if (question4 === "correct"){
+		//correct counter increases by 1 and stores its value
+		counter.correct++;
+	}
+	//else if value of question 3 is wrong:
+	else if (question4 === "wrong"){
+		//wrong counter increases by 1 and stores its value
+		counter.wrong++;
+	}
+	//else no values match:
+	else {
+		//then unanswered increases by 1 and stores its value
+		counter.unanswered++;
+	};
+
+	//---Question 5 Check---
+	//if value of question5 is correct:
+	if (question5 === "correct"){
+		//correct counter increases by 1 and stores its value
+		counter.correct++;
+	}
+	//else if value of question 3 is wrong:
+	else if (question5 === "wrong"){
+		//wrong counter increases by 1 and stores its value
+		counter.wrong++;
+	}
+	//else no values match:
+	else {
+		//then unanswered increases by 1 and stores its value
+		counter.unanswered++;
+	};
+
+
+	//clear timer div
+	$("#startTimer").html("");
 	//#maintext-Container is replaced wtih final scores
 	$("#maintext-Container").html("<h2>" + "</h2>");
 	$("#maintext-Container").addClass("text-center");
 	//cycle through the counter object and append the final scores
 	$("#maintext-Container").append("<h2> Correct: " + counter.correct + "</h2>");
 	$("#maintext-Container").append("<h2> Wrong: " + counter.wrong + "</h2>");
-	$("#maintext-Container").append("<h2> Unanswered: " + counter.unanswered + "</h2>");
+	$("#maintext-Container").append("<h2> Unanswered: " + counter.unanswered + "</h2>" + "<br>");
 
 	//#startTimer is replaced with a gif
 	//great job gif
-    if (counter.correct >= 5){
-
-    	$("#startTimer").html(
+    if (counter.correct = 5){
+    	//Big puts on the shoe
+    	$("#maintext-Container").append(
     	"<img src='https://i.giphy.com/media/p9MrGLrUDrU1a/giphy.webp' />"
     	);
 
     //meh job gif
     }else if (counter.correct >= 3){
-
+    	//Black dresses
+    	$("#maintext-Container").append(
+    	"<img src='https://media.giphy.com/media/4yRAUN2XxuQec/giphy.gif' />"
+    	);
 
     //womp womp gif
+    }else if (counter.correct >= 1){
+    	//Sideways looks
+    	$("#maintext-Container").append(
+    	"<img src='https://media.giphy.com/media/caH3HQRogbU0U/giphy.gif' />"
+    	);
+
     }else if (counter.correct = 0){
+    	//Valium quote
+    	$("#maintext-Container").append(
+    	"<img src='https://media1.popsugar-assets.com/files/thumbor/0Wzb6VuyFJm28bl22FjfNxRA8iE/fit-in/1200x630/filters:format_auto-!!-:strip_icc-!!-:fill-!white!-/2015/03/25/964/n/1922283/ee2540cdcfd41388_sex-and-the-city-celebrity-watchdog-3/i/Blissful-Statement.gif' />"
+    	);
+
 
 
     }
